ef24a1faef8a5d5702fe4f35800e68648d6f96ba
Hieu Phan <ptrhieu.it@gmail.com>
2017-09-20 16:40:27 +0700

    allow cors

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,5 @@ module Ezylearning
                  :methods => [:get, :post, :options, :delete, :put]
       end
     end
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
-      end
-    end
   end
 end
