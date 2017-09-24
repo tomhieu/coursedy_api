e7b5a27cc0f1ad8e907401b582f6eac3c19110e8
Hieu Phan <ptrhieu.it@gmail.com>
2017-09-24 09:30:31 +0700

    fix user serializer

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ module Api
 
       def current_api_user
         if current_user
-          render json: current_user, each_serializer: UsersSerializer
+          render json: current_user, serializer: UsersSerializer
         else
           render_error_response('not login', :not_found)
         end
