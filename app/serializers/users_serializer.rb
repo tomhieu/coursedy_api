754779524960bd9db1a1e925a728d670c424a3e5
Hieu Phan <ptrhieu.it@gmail.com>
2017-11-14 00:17:37 +0700

    Revert "update user serializer"
    This reverts commit 7e4813f8531078884a83a7d36acb9b08b8bda655.

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,5 +1,5 @@
 class UsersSerializer < ActiveModel::Serializer
-  attributes :id, :name, :phone_number, :email, :roles, :address, :date_of_birth, :gender, :uid, :provider
+  attributes :id, :name, :phone_number, :email, :roles, :address, :date_of_birth, :gender
 
   def roles
     object.roles.pluck(:name)
