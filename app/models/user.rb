7e4bcd26c830996251571cca590ccb463a3f4827
Hieu Phan <ptrhieu.it@gmail.com>
2018-03-17 16:12:13 +0700

    delete dependency on deleting main object

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ActiveRecord::Base
   has_many :course_subscribers
   has_many :enrolled_courses, source: 'course', through: :participations
   has_many :followed_courses, source: 'course', through: :course_subscribers
-  has_one :tutor
+  has_one :tutor, dependent: :destroy
 
   validates :name, presence: true
   validates :gender, inclusion: ['F', 'M'], allow_nil: true
