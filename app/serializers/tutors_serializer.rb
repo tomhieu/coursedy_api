e769ef2b7380739479587f39d07b9eb2a970b8f3
Hieu Phan <ptrhieu.it@gmail.com>
2018-03-17 12:37:29 +0700

    fix stack error

--- app/serializers/tutors_serializer.rb
+++ app/serializers/tutors_serializer.rb
@@ -2,7 +2,7 @@ class TutorsSerializer < ActiveModel::Serializer
   attributes :id, :title, :description, :user_id, :user, :degrees, :hour_rate, :highest_education, :categories, :currency
 
   def user
-    if @instance_options[:full_info] && user
+    if @instance_options[:full_info] && object.user
       UsersSerializer.new(object.user).to_h
     end
   end
