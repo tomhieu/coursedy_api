d01176e332aaf6b1dde82e67018b9862c8e109ae
Hieu Phan <ptrhieu.it@gmail.com>
2018-04-28 16:05:08 +0700

    education serializer

--- app/serializers/tutor_educations_serializer.rb
+++ app/serializers/tutor_educations_serializer.rb
@@ -1,3 +1,11 @@
 class TutorEducationsSerializer < ActiveModel::Serializer
-  attributes :id, :title, :graduated_from, :start_date, :end_date, :description
+  attributes :id, :title, :graduated_from, :start_date, :end_date, :description, :tutor_id
+
+  def start_date
+    object.start_date.strftime('%d/%m/%Y') if object.start_date
+  end
+
+  def end_date
+    object.end_date.strftime('%d/%m/%Y') if object.end_date
+  end
 end
