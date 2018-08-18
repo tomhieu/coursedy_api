7a1d6ccc8561ee58393a8433049d154351c94d3b
Hieu Phan <ptrhieu.it@gmail.com>
2018-08-18 11:42:56 +0700

    fix tutor review serializer

--- app/serializers/tutor_reviews_serializer.rb
+++ app/serializers/tutor_reviews_serializer.rb
@@ -2,6 +2,6 @@ class TutorReviewsSerializer < ActiveModel::Serializer
   attributes :id, :content, :user_id, :user, :teacher_id, :created_at, :updated_at
 
   def user
-    UsersSerializer.new(object.tutor).to_h
+    UsersSerializer.new(object.user).to_h
   end
 end
