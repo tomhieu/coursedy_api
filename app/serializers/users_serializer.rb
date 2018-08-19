0aeed8cc73c5451c4fcbe5412f0f661153dd5c23
Hieu Phan <ptrhieu.it@gmail.com>
2018-08-19 11:05:52 +0700

    add balance to current user

--- app/serializers/users_serializer.rb
+++ app/serializers/users_serializer.rb
@@ -1,5 +1,9 @@
 class UsersSerializer < ActiveModel::Serializer
-  attributes :id, :name, :phone_number, :email, :roles, :address, :date_of_birth, :gender, :avatar, :rating_count, :rating_points, :created_at, :updated_at
+  attributes :id, :name, :phone_number, :email, :balance, :roles, :address, :date_of_birth, :gender, :avatar, :rating_count, :rating_points, :created_at, :updated_at
+
+  def balance
+    object.account.balance
+  end
 
   def roles
     object.roles.pluck(:name)
