a945d3a195c2bce06cbbf2cef27ec432c7d783dc
Hieu Phan <ptrhieu.it@gmail.com>
2018-08-19 17:05:08 +0700

    search tutor with status

--- app/controllers/api/v1/tutors_controller.rb
+++ app/controllers/api/v1/tutors_controller.rb
@@ -17,7 +17,7 @@ module Api
             with(:category_id, params[:categories])
           end
 
-          with :verification_status, 2
+          with :status, 2
         end
 
         @tutors = paginate Tutor.where(id: solr_search.results.map(&:id)).includes(:user, :categories, :degrees)
