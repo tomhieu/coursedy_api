6e0ae8955da403c4f3c33a3bd7171b143a6b2662
Hieu Phan <ptrhieu.it@gmail.com>
2018-09-08 12:33:17 +0700

    unscope in relations

--- app/models/course_section.rb
+++ app/models/course_section.rb
@@ -1,13 +1,6 @@
 class CourseSection < ApplicationRecord
-  belongs_to :course, required: false
+  belongs_to :course, -> { unscoped.where(id: self.course_id).first }
   has_many :lessons, dependent: :destroy
 
   validates :title, presence: true
-
-  validate :check_course
-
-  private
-  def check_course
-    errors.add(:course_id, 'is required') if !self.course_id || !Course.unscoped.where(id: self.course_id).exists?
-  end
 end
