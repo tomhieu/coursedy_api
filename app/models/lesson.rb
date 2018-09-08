903f107716d96a0f305665d7b5f07f59080de2f2
Hieu Phan <ptrhieu.it@gmail.com>
2018-09-08 12:21:34 +0700

    check course when creating lessons

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,5 +1,5 @@
 class Lesson < ApplicationRecord
-  belongs_to :course
+  belongs_to :course, required: false
   belongs_to :course_section
   has_many :documents, dependent: :destroy
 
@@ -12,8 +12,13 @@ class Lesson < ApplicationRecord
   after_create :update_lesson_count
 
   validates :title, presence: true
+  validate :check_course
 
   private
+
+  def check_course
+    errors.add(:course_id, 'is required') if !self.course_id || !Course.unscoped.where(id: self.course_id).exists?
+  end
 
   def belong_to_course
     errors.add(:course_id, I18n.t('common_errors.invalid_course_id')) if self.course_section.course_id != self.course_id
