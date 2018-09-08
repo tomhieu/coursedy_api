336fb058baec4c70d20ccb714f25c764b783c711
Hieu Phan <ptrhieu.it@gmail.com>
2018-09-08 11:25:51 +0700

    update course validate

--- app/models/course_section.rb
+++ app/models/course_section.rb
@@ -1,6 +1,13 @@
 class CourseSection < ApplicationRecord
-  belongs_to :course
+  belongs_to :course, required: false
   has_many :lessons, dependent: :destroy
 
   validates :title, presence: true
+
+  validate :check_course
+
+  private
+  def check_course
+    errors.add('course is required') if !self.course_id || Course.unscoped.where(id: self.course_id).exists?
+  end
 end
