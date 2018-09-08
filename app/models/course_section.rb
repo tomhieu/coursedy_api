526b8868eb0231b16f5c89a8daf0a8d92e392a31
Hieu Phan <ptrhieu.it@gmail.com>
2018-09-08 11:45:22 +0700

    fix model error

--- app/models/course_section.rb
+++ app/models/course_section.rb
@@ -8,6 +8,6 @@ class CourseSection < ApplicationRecord
 
   private
   def check_course
-    errors.add('course is required') if !self.course_id || Course.unscoped.where(id: self.course_id).exists?
+    errors.add(:course_id, 'is required') if !self.course_id || !Course.unscoped.where(id: self.course_id).exists?
   end
 end
