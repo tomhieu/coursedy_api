9648dea5aea36278eec8a60edcae2b536007f512
Hieu Phan <ptrhieu.it@gmail.com>
2018-11-18 16:47:46 +0700

    puma variable

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -12,6 +12,16 @@ set :rails_env, :production
 # role-based syntax
 # ==================
 
+set :puma_threads,    [4, 16]
+set :puma_workers, 2
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.error.log"
+set :puma_error_log,  "#{release_path}/log/puma.access.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true  # Change to false when not using ActiveRecord
 # Defines a role with one or multiple servers. The primary server in each
 # group is considered to be the first unless any hosts have the primary
 # property set. Specify the username and a domain or IP for the server.
