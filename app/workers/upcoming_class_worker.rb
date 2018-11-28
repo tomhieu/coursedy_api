b5ae6ff475c95cf2b3db062bf2bf7105c7a9c988
Hieu Phan <ptrhieu.it@gmail.com>
2018-11-28 13:23:15 +0700

    calculate time in Hanoi timezone

--- app/workers/upcoming_class_worker.rb
+++ app/workers/upcoming_class_worker.rb
@@ -5,7 +5,7 @@ class UpcomingClassWorker
     course_ids = Course.where(status: 'started', verification_status: Course::APPROVED).pluck(:id)
     current_wday = WeekDaySchedule.days.to_a.map(&:reverse).to_h[Time.current.wday]
     WeekDaySchedule.where(course_id: course_ids).where(day: current_wday).find_each do |wd|
-      period = (wd.start_time_str.to_time - Time.current)/60
+      period = (wd.start_time_str.in_time_zone(AppSettings.time_zone) - Time.current)/60
       if period > 0 && period <= 15
         UpcomingClassNotificationWorker.perform_async(wd.course_id)
       end
