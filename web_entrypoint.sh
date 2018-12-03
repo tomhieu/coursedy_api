dc2909db9df9e9cd10e42fc002dabae4d4d4020b
Hieu Phan <ptrhieu.it@gmail.com>
2018-12-04 01:48:27 +0700

    reindex solr on start up

--- web_entrypoint.sh
+++ web_entrypoint.sh
@@ -9,6 +9,7 @@ rm /etc/nginx/sites-enabled/*
 rm /etc/nginx/sites-available/*
 ln -s /coursedy/nginx_conf/nginx.conf /etc/nginx/sites-enabled/coursedy_api.conf
 
+bundle exec rake sunspot:reindex
 bundle exec rake assets:precompile
 bundle exec puma -C config/puma.rb -d
 
