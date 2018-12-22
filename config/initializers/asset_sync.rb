2e4ded207a3c5c080e0677e23849290721cccf9c
Hieu Phan <ptrhieu.it@gmail.com>
2018-12-23 02:46:43 +0700

    asset sync config

--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -22,7 +22,7 @@ if defined?(AssetSync)
 
     # Increase upload performance by configuring your region
     config.fog_region = 'ap-southeast-1'
-    config.aws_iam_roles = true
+    config.aws_iam_roles = false
     #
     # Set `public` option when uploading file depending on value,
     # Setting to "default" makes asset sync skip setting the option
