d84d12ecea925bb73fb747f72dc2fb72bbea102e
Hieu Phan <ptrhieu.it@gmail.com>
2018-12-22 16:05:46 +0700

    only allow user with role admin to access admin dashboard

--- app/controllers/cd_admin/admin_controller.rb
+++ app/controllers/cd_admin/admin_controller.rb
@@ -1,7 +1,13 @@
 class CdAdmin::AdminController < ActionController::Base
   layout 'application'
   before_action :authenticate_cd_admin_user!
+  before_action :check_admin
   before_action :check_duo
+
+  private
+  def check_admin
+    redirect_to '/404.html' unless current_cd_admin_user.has_role? :admin
+  end
 
   def check_duo
     redirect_to cd_admin_duo_new_path unless session[:duo_user]
