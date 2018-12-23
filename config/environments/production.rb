31274ae34bc90b7b1fd6dc8d084ec1b3071582d7
Hieu Phan <ptrhieu.it@gmail.com>
2018-12-23 20:29:11 +0700

    fix s3 asset host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}s3-ap-southeast-1.amazonaws.com"
+  config.action_controller.asset_host = "//#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com"
   config.assets.prefix = "/production/assets"
   config.assets.digest = true
   config.assets.enabled = true
